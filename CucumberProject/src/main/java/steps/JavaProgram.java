2f65b2720e63d7cdc36074f3e6dcc281a4511c5d
Nilesh <Nilesh@Neel>
2025-07-13 23:16:14 +0530

    adding java program ,2nd commit

---
+++ CucumberProject/src/main/java/steps/JavaProgram.java
@@ -0,0 +1,27 @@
+package steps;
+
+import java.util.Arrays;
+
+public class JavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = Integer.MAX_VALUE;
+		int[] arr = {3,5,2,7,9};
+		
+		Arrays.sort(arr);
+		
+		int secondmax = arr[arr.length-2];
+		
+		System.out.println(secondmax);
+		
+		
+		
+		
+	
+		System.out.println(a);
+	}
+
+}
